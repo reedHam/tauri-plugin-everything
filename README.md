369053ef7bb432f5f9bbab259a2c197b66d1f149
Reed <reed.hambrook@gmail.com>
2023-07-23 16:31:38 -0700

    forwarded basic commands

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tauri Plugin everything
+
+Allow your Tauri app to access [Void Tools](https://www.voidtools.com/) [Everything](https://www.voidtools.com/support/everything/) search engine.
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,252 @@
-
-use serde::{ser::Serializer, Serialize};
+use serde::{ser::Serializer, Deserialize, Serialize};
 use tauri::{
-  command,
-  plugin::{Builder, TauriPlugin},
-  AppHandle, Manager, Runtime, State, Window,
+    command,
+    plugin::{Builder, TauriPlugin},
+    AppHandle, Manager, Runtime, State, Window,
 };
 
-use std::{collections::HashMap, sync::Mutex};
-
-type Result<T> = std::result::Result<T, Error>;
+use everything_rs::{Everything, EverythingError, EverythingRequestFlags, EverythingSort};
+use std::sync::RwLock;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-  #[error(transparent)]
-  Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Everything(#[from] EverythingError),
+
+    #[error("Lock poisoned")]
+    Poisoned,
+
+    #[error("Invalid Request Flags")]
+    InvalidRequestFlags,
 }
+
+impl From<std::sync::PoisonError<std::sync::RwLockWriteGuard<'_, Everything>>> for Error {
+    fn from(_: std::sync::PoisonError<std::sync::RwLockWriteGuard<'_, Everything>>) -> Self {
+        Self::Poisoned
+    }
+}
+
+impl From<std::sync::PoisonError<std::sync::RwLockReadGuard<'_, Everything>>> for Error {
+    fn from(_: std::sync::PoisonError<std::sync::RwLockReadGuard<'_, Everything>>) -> Self {
+        Self::Poisoned
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
 
 impl Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-  where
-    S: Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
 
-#[derive(Default)]
-struct MyState(Mutex<HashMap<String, String>>);
+struct EverythingState(RwLock<Everything>);
+
+impl Default for EverythingState {
+    fn default() -> Self {
+        Self(RwLock::new(Everything::new()))
+    }
+}
 
 #[command]
-async fn execute<R: Runtime>(
-  _app: AppHandle<R>,
-  _window: Window<R>,
-  state: State<'_, MyState>,
+async fn set_search<R: Runtime>(
+    _app: AppHandle<R>,
+    _window: Window<R>,
+    state: State<'_, EverythingState>,
+    query: String,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    ev.set_search(&query);
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum RequestFlags {
+    FileName,
+    Path,
+    FullPathAndFileName,
+    Extension,
+    Size,
+    DateCreated,
+    DateModified,
+    DateAccessed,
+    Attributes,
+    FileListFileName,
+    RunCount,
+    DateRun,
+    DateRecentlyChanged,
+    HighlightedFileName,
+    HighlightedPath,
+    HighlightedFullPathAndFileName,
+}
+
+impl From<RequestFlags> for EverythingRequestFlags {
+    fn from(value: RequestFlags) -> Self {
+        match value {
+            RequestFlags::FileName => EverythingRequestFlags::FileName,
+            RequestFlags::Path => EverythingRequestFlags::Path,
+            RequestFlags::FullPathAndFileName => EverythingRequestFlags::FullPathAndFileName,
+            RequestFlags::Extension => EverythingRequestFlags::Extension,
+            RequestFlags::Size => EverythingRequestFlags::Size,
+            RequestFlags::DateCreated => EverythingRequestFlags::DateCreated,
+            RequestFlags::DateModified => EverythingRequestFlags::DateModified,
+            RequestFlags::DateAccessed => EverythingRequestFlags::DateAccessed,
+            RequestFlags::Attributes => EverythingRequestFlags::Attributes,
+            RequestFlags::FileListFileName => EverythingRequestFlags::FileListFileName,
+            RequestFlags::RunCount => EverythingRequestFlags::RunCount,
+            RequestFlags::DateRun => EverythingRequestFlags::DateRun,
+            RequestFlags::DateRecentlyChanged => EverythingRequestFlags::DateRecentlyChanged,
+            RequestFlags::HighlightedFileName => EverythingRequestFlags::HighlightedFileName,
+            RequestFlags::HighlightedPath => EverythingRequestFlags::HighlightedPath,
+            RequestFlags::HighlightedFullPathAndFileName => {
+                EverythingRequestFlags::HighlightedFullPathAndFileName
+            }
+        }
+    }
+}
+
+#[command]
+async fn set_request_flags(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    flags: Vec<RequestFlags>,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    let mut flags = flags;
+    if let Some(flag) = flags.pop() {
+        let mut flags = EverythingRequestFlags::from(flag);
+        for flag in flags {
+            flags |= EverythingRequestFlags::from(flag);
+        }
+        ev.set_request_flags(flags);
+        Ok(())
+    } else {
+        Err(Error::InvalidRequestFlags)
+    }
+}
+
+#[command]
+async fn set_sort(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    sort: EverythingSort,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    ev.set_sort(sort);
+    Ok(())
+}
+
+#[command]
+async fn set_result_offset(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    offset: u32,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    ev.set_result_offset(offset);
+    Ok(())
+}
+
+#[command]
+async fn set_max_results(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    max_results: u32,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    ev.set_max_results(max_results);
+    Ok(())
+}
+
+#[command]
+async fn query(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+) -> Result<()> {
+    let ev = state.0.write()?;
+    ev.query()?;
+    Ok(())
+}
+
+#[command]
+async fn get_num_results(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+) -> Result<u32> {
+    let ev = state.0.read()?;
+    Ok(ev.get_num_results())
+}
+
+#[command]
+async fn get_full_path_results(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+) -> Result<Vec<String>> {
+    let ev = state.0.read()?;
+    Ok(ev.full_path_iter().flatten().collect())
+}
+
+#[command]
+async fn get_result_full_path(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    index: u32,
 ) -> Result<String> {
-  state.0.lock().unwrap().insert("key".into(), "value".into());
-  Ok("success".to_string())
+    let ev = state.0.read()?;
+    Ok(ev.get_result_full_path(index)?)
+}
+
+#[command]
+async fn get_file_name_results(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+) -> Result<Vec<String>> {
+    let ev = state.0.read()?;
+    Ok(ev.name_iter().flatten().collect())
+}
+
+#[command]
+async fn get_result_file_name(
+    _app: AppHandle<impl Runtime>,
+    _window: Window<impl Runtime>,
+    state: State<'_, EverythingState>,
+    index: u32,
+) -> Result<String> {
+    let ev = state.0.read()?;
+    Ok(ev.get_result_file_name(index)?)
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("everything")
-    .invoke_handler(tauri::generate_handler![execute])
-    .setup(|app| {
-      app.manage(MyState::default());
-      Ok(())
-    })
-    .build()
+    Builder::new("everything")
+        .invoke_handler(tauri::generate_handler![
+            set_search,
+            set_request_flags,
+            set_sort,
+            set_result_offset,
+            set_max_results,
+            query,
+            get_num_results,
+            get_full_path_results,
+            get_result_full_path,
+            get_file_name_results,
+            get_result_file_name
+        ])
+        .setup(|app| {
+            app.manage(EverythingState::default());
+            Ok(())
+        })
+        .build()
 }
